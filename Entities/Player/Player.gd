d43971fed0bdc0bcb0f1adad8621d0f4f28c14f4
stephenus <stephenus@users.noreply.github.com>
2020-10-25 19:44:08 +0200

    GUI system (Tutorial 9)

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -2,9 +2,32 @@ extends KinematicBody2D
 
 export var speed = 75
 
+var health = 100
+var health_max = 100
+var health_regeneration = 1
+var mana = 100
+var mana_max = 100
+var mana_regeneration = 2
+
 var last_direction = Vector2(0, 1)
 
 var attack_playing = false
+
+signal player_stats_changed
+
+func _ready():
+	emit_signal("player_stats_changed", self)
+	
+func _process(delta):	
+	var new_mana = min(mana + mana_regeneration * delta, mana_max)
+	if new_mana != mana:
+		mana = new_mana
+		emit_signal("player_stats_changed", self)
+	
+	var new_health = min(health + health_regeneration * delta, health_max)
+	if new_health != health:
+		health = new_health
+		emit_signal("player_stats_changed", self)
 
 func _physics_process(delta):
 	var direction: Vector2
@@ -49,9 +72,12 @@ func _input(event):
 		var animation = get_animation_direction(last_direction) + "_attack"
 		$Sprite.play(animation)
 	elif event.is_action_pressed("fireball"):
-		attack_playing = true
-		var animation = get_animation_direction(last_direction) + "_fireball"
-		$Sprite.play(animation)
+		if mana >= 25:
+			mana = mana - 25
+			emit_signal("player_stats_changed", self)
+			attack_playing = true
+			var animation = get_animation_direction(last_direction) + "_fireball"
+			$Sprite.play(animation)
 
 
 func _on_Sprite_animation_finished() -> void:
