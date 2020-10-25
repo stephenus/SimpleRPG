fbb2979cbc4d20cf2f12be52d058465d05c10ce2
stephenus <stephenus@users.noreply.github.com>
2020-10-25 20:56:01 +0200

    Spawning and AI fixes

--- Entities/Skeleton/Skeleton.gd
+++ Entities/Skeleton/Skeleton.gd
@@ -8,12 +8,32 @@ export var speed = 25
 var direction : Vector2
 var last_direction = Vector2(0, 1)
 var bounce_countdown = 0
-
+var other_animation_playing = false
 
 func _ready() -> void:
 	player = get_tree().root.get_node("Root/Player")
 	rnd.randomize()
+	
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
 
+func animates_monster(direction: Vector2):
+	if direction != Vector2.ZERO:
+		last_direction = direction		
+		var animation = get_animation_direction(last_direction) + "_walk"
+		$AnimatedSprite.play(animation)
+	else:		
+		var animation = get_animation_direction(last_direction) + "_idle"
+		$AnimatedSprite.play(animation)
 
 func _on_Timer_timeout():
 	var player_relative_position = player.position - position
@@ -41,3 +61,17 @@ func _physics_process(delta):
 	if collision != null and collision.collider.name != "Player":
 		direction = direction.rotated(rnd.randf_range(PI/4, PI/2))
 		bounce_countdown = rnd.randi_range(2, 5)
+		
+	if not other_animation_playing:
+		animates_monster(direction)
+
+func arise():
+	other_animation_playing = true
+	$AnimatedSprite.play("birth")
+
+
+func _on_AnimatedSprite_animation_finished() -> void:
+	if $AnimatedSprite.animation == "birth":
+		$AnimatedSprite.animation = "down_idle"
+		$Timer.start()
+	other_animation_playing = false
---
+++ Entities/SkeletonSpawner/SkeletonSpawner.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+var tilemap
+var tree_tilemap
+
+export var spawn_area : Rect2 = Rect2(50, 150, 700, 700)
+export var max_skeletons = 100
+export var start_skeletons = 10
+var skeleton_count = 0
+var skeleton_scene = preload("res://Entities/Skeleton/Skeleton.tscn")
+
+var rnd = RandomNumberGenerator.new()
+
+func _ready():	
+	tilemap = get_tree().root.get_node("Root/TileMap")
+	tree_tilemap = get_tree().root.get_node("Root/TreeTileMap")
+	
+	rnd.randomize()
+	
+	for i in range(start_skeletons):
+		instance_skeleton()
+	skeleton_count = start_skeletons
+
+func test_position(position : Vector2):
+	var cell_coord = tilemap.world_to_map(position)
+	var cell_type_id = tilemap.get_cellv(cell_coord)
+	var grass_or_sand = (cell_type_id == tilemap.tile_set.find_tile_by_name("Grass")) || (cell_type_id == tilemap.tile_set.find_tile_by_name("Sand"))
+		
+	cell_coord = tree_tilemap.world_to_map(position)
+	cell_type_id = tree_tilemap.get_cellv(cell_coord)
+	var no_trees = (cell_type_id != tilemap.tile_set.find_tile_by_name("Tree"))
+	
+	return grass_or_sand and no_trees
+
+func instance_skeleton():
+	var skeleton = skeleton_scene.instance()
+	add_child(skeleton)
+	
+	var valid_position = false
+	while not valid_position:
+		skeleton.position.x = spawn_area.position.x + rnd.randf_range(0, spawn_area.size.x)
+		skeleton.position.y = spawn_area.position.y + rnd.randf_range(0, spawn_area.size.y)
+		valid_position = test_position(skeleton.position)
+	
+	skeleton.arise()
+
+
+func _on_Timer_timeout() -> void:
+	if skeleton_count < max_skeletons:
+		instance_skeleton()
+		skeleton_count = skeleton_count + 1
