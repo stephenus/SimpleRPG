17ee486cd90f1146b154dc0d329e0641ab4c5c0d
stephenus <stephenus@users.noreply.github.com>
2020-10-25 21:08:44 +0200

    Added cooldown time to player attack

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -13,6 +13,10 @@ var last_direction = Vector2(0, 1)
 
 var attack_playing = false
 
+var attack_cooldown_time = 500
+var next_attack_time = 0
+var attack_damage = 30
+
 signal player_stats_changed
 
 func _ready():
@@ -68,9 +72,14 @@ func get_animation_direction(direction: Vector2):
 	
 func _input(event):
 	if event.is_action_pressed("attack"):
-		attack_playing = true
-		var animation = get_animation_direction(last_direction) + "_attack"
-		$Sprite.play(animation)
+		var now = OS.get_ticks_msec()
+		if now >= next_attack_time:
+			# Play attack animation
+			attack_playing = true
+			var animation = get_animation_direction(last_direction) + "_attack"
+			$Sprite.play(animation)
+			# Add cooldown time to current time
+			next_attack_time = now + attack_cooldown_time
 	elif event.is_action_pressed("fireball"):
 		if mana >= 25:
 			mana = mana - 25
